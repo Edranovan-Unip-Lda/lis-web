f5fc57120b95665e9e40eb766a51e8f2d27b6d2e
Geovannio Frederico de Jesus Vinhas <geovanniovinhas@live.com>
2026-01-14 00:12:07 +0900

    Add navigation progress bar to auth layout
    Introduced a progress bar that displays during router navigation events in the auth layout component. This improves user feedback by showing an indeterminate progress bar while navigation is in progress.

--- src/app/pages/auth/auth-layout/auth-layout.component.ts
+++ src/app/pages/auth/auth-layout/auth-layout.component.ts
@@ -1,10 +1,11 @@
 import { LayoutService } from '@/layout/service/layout.service';
 import { Component, inject } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Event, NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router, RouterModule } from '@angular/router';
+import { ProgressBar } from 'primeng/progressbar';
 
 @Component({
   selector: 'app-auth-layout',
-  imports: [RouterModule],
+  imports: [RouterModule, ProgressBar],
   templateUrl: './auth-layout.component.html',
   styleUrl: './auth-layout.component.scss'
 })
@@ -12,6 +13,33 @@ export class AuthLayoutComponent {
   layoutService = inject(LayoutService);
   readonly startYear = 2026;
   readonly currentYear = new Date().getFullYear();
+  showProgressBar = false;
+
+  constructor(
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.router.events.subscribe((event: Event) => {
+
+      switch (true) {
+        case event instanceof NavigationStart: {
+          this.showProgressBar = true;
+          break;
+        }
+
+        case event instanceof NavigationEnd:
+        case event instanceof NavigationCancel:
+        case event instanceof NavigationError: {
+          this.showProgressBar = false;
+          break;
+        }
+        default: {
+          break;
+        }
+      }
+    });
+  }
 
   get copyrightYears(): string {
     return this.currentYear > this.startYear
