aee163b3cc063aa4acfa5d217c4ca1cbf6ce91a2
Geovannio Frederico de Jesus Vinhas <geovanniovinhas@live.com>
2026-01-23 17:01:19 +0900

    Update reCAPTCHA site key in environment config
    Replaces the old reCAPTCHA site key with a new one in the environment.ts file. This ensures the application uses the correct key for reCAPTCHA validation.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
     url: 'https://lic.mci.gov.tl/api',
     apiUrl: 'https://lic.mci.gov.tl/api/api/v1',
     webUrl: 'https://lic.mci.gov.tl',
-    recaptchaSiteKey: '6Ld3TEssAAAAACZ-r5qH5eXOLmdtEKTPhTXxMklC',
+    recaptchaSiteKey: '6LfklVMsAAAAAFGCH8C4OnEY9GbZIkXj_s2Fw39-',
     sentryEnv: 'production'
 };
